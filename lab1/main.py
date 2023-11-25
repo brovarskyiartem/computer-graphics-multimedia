a64162a55c7e118bc3b2c2b1a208d45fe3fdba2b
brovarskyiartem <145754242+brovarskyiartem@users.noreply.github.com>
2023-11-25 13:57:22 +0200

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -1,11 +1,8 @@
 from PIL import Image, ImageDraw
+import math
 
 # Створення пустого списку для зберігання даних
 datapoints = []
-
-# Створення білого зображення розміром 960x540 пікселів
-image = Image.new("RGB", [960, 540], color="white")  # Змінено розмір для обернутого зображення
-draw = ImageDraw.Draw(image)
 
 # Зчитування даних з файлу 'DS0.txt' та додавання їх до списку datapoints
 with open('DS0.txt', 'r') as file:
@@ -15,14 +12,50 @@ with open('DS0.txt', 'r') as file:
         parts = line.strip().split()
         x, y = map(int, parts)
         datapoints.append((x, y))
-point_radius = 0.1  # Розмір точок
-point_color = 'black'  # Колір точок
-# Виведення зчитаних даних у вигляді кортежу та обертання координат
+
+# Визначення кута обертання в радіанах
+alpha = math.radians(10)
+
+# Визначення матриці обертання
+h, k = 480, 480
+rotation_matrix = [
+    [math.cos(alpha), -math.sin(alpha), (1 - math.cos(alpha)) * h + math.sin(alpha) * k],
+    [math.sin(alpha), math.cos(alpha), -math.sin(alpha) * h + (1 - math.cos(alpha)) * k],
+    [0, 0, 1]
+]
+
+# Виконання афінного перетворення
+transformed_datapoints = []
 for point in datapoints:
     x, y = point
-    x, y = y,520 - x  # Обертання координат на 90 градусів
+    # Додавання третьої координати (1) для виконання матричного множення
+    point_matrix = [[x], [y], [1]]
+    result_matrix = [[0], [0], [0]]
+
+    # Матричне множення
+    for i in range(len(rotation_matrix)):
+        for j in range(len(point_matrix[0])):
+            for k in range(len(point_matrix)):
+                result_matrix[i][j] += rotation_matrix[i][k] * point_matrix[k][j]
+
+    # Зберігання трансформованих точок
+    transformed_datapoints.append((result_matrix[0][0], result_matrix[1][0]))
+
+# Створення зображення та області малювання
+image = Image.new("RGB", [960, 960], color="white")
+draw = ImageDraw.Draw(image)
+
+point_radius = 0.1  # Розмір точок
+point_color = 'blue'  # Колір точок після трансформації
+
+# Виведення трансформованих точок
+for point in transformed_datapoints:
+    x, y = point
+    x, y = int(y), int(520 - x)  # Обертання координат на 90 градусів
     x, y = x - point_radius, y - point_radius  # Центруємо точки
     x1, y1 = x + 2 * point_radius, y + 2 * point_radius  # Розраховуємо прямокутник, який включає точку
     draw.ellipse([x, y, x1, y1], fill=point_color, outline=point_color)
-image.save("res.png")
+
+# Збереження результатів у файл
+image.save("transformed_result.png")
 image.show()  # Покажемо на екрані
